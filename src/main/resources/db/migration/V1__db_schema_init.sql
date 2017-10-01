1921b02d555a6d01ef4a9caa9276a3220660b192
wuwenlan <wuwenlan@nonobank.com>
2017-10-01 15:53:41 +0800

    添加主键

--- src/main/resources/db/migration/V1__db_schema_init.sql
+++ src/main/resources/db/migration/V1__db_schema_init.sql
@@ -1,20 +1,19 @@
 -- 用户信息表
 CREATE TABLE user_info
 (
-  id          BIGINT
-    PRIMARY KEY,
+  ID INTEGER PRIMARY KEY ,
   user_name   VARCHAR(50),
   mobile_num  VARCHAR(20),
   nick_name   VARCHAR(50),
   is_enable   INT DEFAULT 1,
   update_time VARCHAR(30),
   create_time VARCHAR(30)
-);
+) ;
 
 -- 用户账户信息
 CREATE TABLE user_finance_account
 (
-  id      BIGINT PRIMARY KEY,
+  id      INTEGER PRIMARY KEY ,
   balance DECIMAL NOT NULL DEFAULT 0.0,
   locking DECIMAL NOT NULL DEFAULT 0.0,
   version INT DEFAULT 1,
@@ -24,8 +23,7 @@ CREATE TABLE user_finance_account
 
 -- admin info信息
 CREATE TABLE admin_info (
- id          BIGINT
-    PRIMARY KEY,
+  id          INTEGER  PRIMARY KEY ,
   admin_name   VARCHAR(50),
   admin_code   VARCHAR(50),
   password     VARCHAR(50),
@@ -34,4 +32,4 @@ CREATE TABLE admin_info (
   update_time VARCHAR(30),
   create_time VARCHAR(30)
 
-);
+)
