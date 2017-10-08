6b6bfaebe6d7a188a7bb3b9436ddc4a38e84566b
wuwenlan <wuwenlan@nonobank.com>
2017-10-08 21:45:11 +0800

    提供接口，更新代码

--- src/main/java/digitalgame/service/UserFinanceAccountService.java
+++ src/main/java/digitalgame/service/UserFinanceAccountService.java
@@ -8,6 +8,15 @@ public interface UserFinanceAccountService {
 
     int updateByPrimaryKeySelective(UserAccountVo UserAccountVo);
 
+    /****
+     *
+     * @param userId 用户ID
+     * @param money  金额
+     * @param type   类型 ，2充值，3位扣减
+     * @return
+     */
+    int updateBalanceByUserId(int userId,double money,int type);
+
 
 
 }
