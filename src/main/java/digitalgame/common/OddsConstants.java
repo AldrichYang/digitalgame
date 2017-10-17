fbf6d6cb239296e1fa43aa5d356acbe85504d0d3
sunboy369 <252669998@qq.com>
2017-10-17 22:47:38 +0800

    Merge branch 'master' of https://github.com/AldrichYang/digitalgame
    # Conflicts:
    #	src/main/resources/templates/starter.html

--- src/main/java/digitalgame/common/OddsConstants.java
+++ src/main/java/digitalgame/common/OddsConstants.java
@@ -1,7 +1,6 @@
 package digitalgame.common;
 
 import digitalgame.model.po.BetInfo;
-import digitalgame.model.po.BetItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -65,87 +64,87 @@ public class OddsConstants {
         }
     }
 
-
-    public List<BetItem> oddsNumber(int i,int j,int k,BetInfo betInfo){
-        List<BetItem> newBetList = new ArrayList<>() ;
-        List<BetItem> betList = betInfo.getBetList();
-        int allNum = i + j + k;
-
-
-        for(BetItem betItem :betList) {
-            int betNum = 0;
-            if ("百大".equals(betItem.getBetName())) {
-                if(isBigNum(k)){
-                    betNum = 2;
-                }
-            } else if ("百小".equals(betItem.getBetName())) {
-                if(!isBigNum(k)){
-                    betNum = 2;
-                }
-            } else if ("百单".equals(betItem.getBetName())) {
-                if(!isBigDouble(k)){
-                    betNum = 2;
-                }
-            } else if ("百双".equals(betItem.getBetName())) {
-                if(isBigDouble(k)){
-                    betNum = 2;
-                }
-            } else if ("十双".equals(betItem.getBetName())) {
-                if(isBigDouble(j)){
-                    betNum = 2;
-                }
-            } else if ("大".equals(betItem.getBetName())) {
-                if(isAllNumBig(allNum)){
-                    betNum = 2;
-                }
-            } else if ("小".equals(betItem.getBetName())) {
-                if(!isAllNumBig(allNum)){
-                    betNum = 2;
-                }
-            } else if ("单".equals(betItem.getBetName())) {
-                if(!isBigDouble(i + j + k)){
-                    betNum = 2;
-                }
-            } else if ("双".equals(betItem.getBetName())) {
-                if(isBigDouble(i + j + k)){
-                    betNum = 2;
-                }
-            } else if ("龙大".equals(betItem.getBetName())) {
-                if(allNum > 19){
-                    betNum = 2;
-                }
-            } else if ("龙小".equals(betItem.getBetName())) {
-                if (allNum < 8){
-                    betNum = 2;
-                }
-            } else if ("龙".equals(betItem.getBetName())) {
-
-            } else if ("小龙小双".equals(betItem.getBetName())) {
-
-            } else if ("全大".equals(betItem.getBetName())) {
-                if(isBigNum(i) && isBigNum(j) && isBigNum(k)){
-                    betNum = 2;
-                }
-            } else if ("全小".equals(betItem.getBetName())) {
-                if(!isBigNum(i) && !isBigNum(j) && !isBigNum(k)){
-                    betNum = 2;
-                }
-            }else if("全单".equals(betItem.getBetName())){
-                if(!isBigDouble(i) && !isBigDouble(j) && !isBigDouble(k)){
-                    betNum = 2;
-                }
-            }else if("全双".equals(betItem.getBetName())){
-                if(isBigDouble(i) && isBigDouble(j) && isBigDouble(k)){
-                    betNum = 2;
-                }
-            }else if("顺子".equals(betItem.getBetName())){
-                if(isShunzi(i,j,k)){
-                    betNum = 20;
-                }
-            }
-            betItem.setReturnMoney(betItem.getBetMoney() * betNum);
-            newBetList.add(betItem);
-        }
-        return newBetList;
-    }
+//
+//    public List<BetItem> oddsNumber(int i,int j,int k,BetInfo betInfo){
+//        List<BetItem> newBetList = new ArrayList<>() ;
+//        List<BetItem> betList = betInfo.getBetList();
+//        int allNum = i + j + k;
+//
+//
+//        for(BetItem betItem :betList) {
+//            int betNum = 0;
+//            if ("百大".equals(betItem.getBetName())) {
+//                if(isBigNum(k)){
+//                    betNum = 2;
+//                }
+//            } else if ("百小".equals(betItem.getBetName())) {
+//                if(!isBigNum(k)){
+//                    betNum = 2;
+//                }
+//            } else if ("百单".equals(betItem.getBetName())) {
+//                if(!isBigDouble(k)){
+//                    betNum = 2;
+//                }
+//            } else if ("百双".equals(betItem.getBetName())) {
+//                if(isBigDouble(k)){
+//                    betNum = 2;
+//                }
+//            } else if ("十双".equals(betItem.getBetName())) {
+//                if(isBigDouble(j)){
+//                    betNum = 2;
+//                }
+//            } else if ("大".equals(betItem.getBetName())) {
+//                if(isAllNumBig(allNum)){
+//                    betNum = 2;
+//                }
+//            } else if ("小".equals(betItem.getBetName())) {
+//                if(!isAllNumBig(allNum)){
+//                    betNum = 2;
+//                }
+//            } else if ("单".equals(betItem.getBetName())) {
+//                if(!isBigDouble(i + j + k)){
+//                    betNum = 2;
+//                }
+//            } else if ("双".equals(betItem.getBetName())) {
+//                if(isBigDouble(i + j + k)){
+//                    betNum = 2;
+//                }
+//            } else if ("龙大".equals(betItem.getBetName())) {
+//                if(allNum > 19){
+//                    betNum = 2;
+//                }
+//            } else if ("龙小".equals(betItem.getBetName())) {
+//                if (allNum < 8){
+//                    betNum = 2;
+//                }
+//            } else if ("龙".equals(betItem.getBetName())) {
+//
+//            } else if ("小龙小双".equals(betItem.getBetName())) {
+//
+//            } else if ("全大".equals(betItem.getBetName())) {
+//                if(isBigNum(i) && isBigNum(j) && isBigNum(k)){
+//                    betNum = 2;
+//                }
+//            } else if ("全小".equals(betItem.getBetName())) {
+//                if(!isBigNum(i) && !isBigNum(j) && !isBigNum(k)){
+//                    betNum = 2;
+//                }
+//            }else if("全单".equals(betItem.getBetName())){
+//                if(!isBigDouble(i) && !isBigDouble(j) && !isBigDouble(k)){
+//                    betNum = 2;
+//                }
+//            }else if("全双".equals(betItem.getBetName())){
+//                if(isBigDouble(i) && isBigDouble(j) && isBigDouble(k)){
+//                    betNum = 2;
+//                }
+//            }else if("顺子".equals(betItem.getBetName())){
+//                if(isShunzi(i,j,k)){
+//                    betNum = 20;
+//                }
+//            }
+//            betItem.setReturnMoney(betItem.getBetMoney() * betNum);
+//            newBetList.add(betItem);
+//        }
+//        return newBetList;
+//    }
 }
--- src/main/java/digitalgame/controller/BetResultController.java
+++ src/main/java/digitalgame/controller/BetResultController.java
@@ -1,10 +1,43 @@
 package digitalgame.controller;
 
+import com.google.common.base.Strings;
+import digitalgame.model.po.BetResult;
+import digitalgame.service.BetResultService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 @RequestMapping("/betResult")
-public class BetResult {
+public class BetResultController {
 
+    @Autowired
+    private BetResultService betResultService;
+
+    @RequestMapping(value = "/betResultList", method = {RequestMethod.GET, RequestMethod.POST})
+    public String getAllUserList(@ModelAttribute BetResult betResult, Model model, HttpServletRequest request) {
+
+        int currentPageNo = 1;
+        if(request != null ){
+            String pageNo = request.getParameter("pageNo");
+            if(!Strings.isNullOrEmpty(pageNo)) currentPageNo = Integer.parseInt(pageNo);
+        }
+
+        int count = betResultService.selectBetSultByPage(0, betResult).size();
+        List<BetResult> betResultsList = betResultService.selectBetSultByPage(currentPageNo, betResult);
+
+        int pageNo = count/10 + (count % 10 == 0 ? 0 : 1);
+        model.addAttribute("betResultsList", betResultsList);
+        model.addAttribute("queryCond",betResult);
+        model.addAttribute("inallPageDesc","总条数："+count+",当前第"+currentPageNo+"页,总共" + pageNo + "页");
+        model.addAttribute("currentPage",currentPageNo);
+        model.addAttribute("inallPage",pageNo);
+        return "betResultList";
+    }
 }
