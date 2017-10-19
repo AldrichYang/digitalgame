bed4867b6259eec7000fd5f17345e5c36d716237
sunboy369 <252669998@qq.com>
2017-10-19 11:47:32 +0800

    中奖结果信息展示

--- src/main/java/digitalgame/service/impl/BetResultServiceImpl.java
+++ src/main/java/digitalgame/service/impl/BetResultServiceImpl.java
@@ -6,6 +6,7 @@ import digitalgame.service.BetResultService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.google.common.base.Strings;
 import java.util.List;
 
 @Service
@@ -17,15 +18,16 @@ public class BetResultServiceImpl implements BetResultService {
     @Override
     public List<BetResult> selectBetSultByPage(int cueerntPage, BetResult betResult) {
         String whereCond = " where 1=1 ";
-//        if(!Strings.isNullOrEmpty(userInfo.getUserName())){
-//            whereCond +=  " and  user_name like '%"+userInfo.getUserName()+"%'";
-//        }
-//        if(!Strings.isNullOrEmpty(userInfo.getNickName())){
-//            whereCond +=  " and nick_name like '%"+userInfo.getNickName()+"%'";
-//        }
+        if(!Strings.isNullOrEmpty(betResult.getBetuser())){
+            whereCond +=  " and  betuser like '%"+betResult.getBetuser()+"%'";
+        }
+        if(!Strings.isNullOrEmpty(betResult.getResultdate())){
+            whereCond +=  " and resultnumber like '%"+betResult.getResultdate()+"%'";
+        }
         if(cueerntPage != 0){
             whereCond += "limit " +((cueerntPage -1) *10) +","+(cueerntPage) * 10;
         }
+
         return betResultMapper.selectByPage(whereCond);
     }
 }
