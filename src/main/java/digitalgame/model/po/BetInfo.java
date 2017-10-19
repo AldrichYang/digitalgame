2428a6532cf620ae079b47daa10b537110892a5d
sunboy369 <252669998@qq.com>
2017-10-19 09:42:41 +0800

    Merge branch 'master' of https://github.com/AldrichYang/digitalgame
    # Conflicts:
    #	src/main/java/digitalgame/SecurityConfig.java
    #	src/main/java/digitalgame/controller/LoginController.java
    #	src/main/java/digitalgame/service/impl/UserDetailServiceImpl.java

--- src/main/java/digitalgame/model/po/BetInfo.java
+++ src/main/java/digitalgame/model/po/BetInfo.java
@@ -1,7 +1,5 @@
 package digitalgame.model.po;
 
-import java.util.List;
-
 /**
  *
  *  下注的实体类
@@ -10,31 +8,69 @@ import java.util.List;
  *
  */
 public class BetInfo {
+  private Long id;
+  private String betman; //投注人
+  private String betitem; //投注项
+  private Double betmoney; //投注金额
+  private Long status; //投注状态 1投注成功 -1已撤销 3投注失败
+  private String createTime;
 
-    public String getBetMan() {
-        return betMan;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setBetMan(String betMan) {
-        this.betMan = betMan;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public List<BetItem> getBetList() {
-        return betList;
-    }
+  public String getBetman() {
+    return betman;
+  }
 
-    public void setBetList(List<BetItem> betList) {
-        this.betList = betList;
-    }
+  public void setBetman(String betman) {
+    this.betman = betman;
+  }
 
-    /**
-     * 投注人名称
-     */
-    private String betMan;
+  public String getBetitem() {
+    return betitem;
+  }
 
-    /**
-     * 投注内容
-     */
-    private List<BetItem> betList;
+  public void setBetitem(String betitem) {
+    this.betitem = betitem;
+  }
+
+  public Double getBetmoney() {
+    return betmoney;
+  }
+
+  public void setBetmoney(Double betmoney) {
+    this.betmoney = betmoney;
+  }
+
+  public Long getStatus() {
+    return status;
+  }
+
+  public void setStatus(Long status) {
+    this.status = status;
+  }
+
+  public String getCreateTime() {
+    return createTime;
+  }
+
+  public void setCreateTime(String createTime) {
+    this.createTime = createTime;
+  }
+
+  public String getUpdateTime() {
+    return updateTime;
+  }
+
+  public void setUpdateTime(String updateTime) {
+    this.updateTime = updateTime;
+  }
+
+  private String updateTime;
 
 }
